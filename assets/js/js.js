2d563991aff8557b6f054c1e8002df950dc2ea2e
Bogdanova <alsu.bogdanova@intel.com>
2025-07-13 21:26:58 +0300

    add tabs

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,37 +3,91 @@ function openModal() {
   myModal.style.display = "flex";
 }
 
-// function closeModal() {
-//   console.log(event);
+function closeModal() {
+  console.log(event);
 
-//   if (
-//     event.target === myModal ||
-//     event.target === document.getElementById("closeModalBtn")
-//   ) {
-//     myModal.style.display = "none";
-//   }
-//   // document.getElementById('myModal').style.display = 'none';
-// }
+  if (
+    event.target === myModal ||
+    event.target === document.getElementById("closeModalBtn")
+  ) {
+    myModal.style.display = "none";
+  }
+  // document.getElementById('myModal').style.display = 'none';
+}
 
-const fruits = ["apple", "banana", "orange"];
+//tabs
+const tabsData = [
+  {
+    sections: [
+      {
+        heading: "Что такое JavaScript?",
+        text: "JavaScript — это язык программирования, используемый для создания интерактивных элементов на веб-страницах.",
+      },
+      {
+        heading: "Зачем он нужен?",
+        text: "С его помощью можно обрабатывать события, изменять HTML и CSS, работать с сервером и многое другое.",
+      },
+    ],
+  },
+  {
+    sections: [
+      {
+        heading: "Функции в JavaScript",
+        text: "Функции позволяют организовать код в переиспользуемые блоки.",
+      },
+      {
+        heading: "Типы функций",
+        text: "Существуют обычные, стрелочные и анонимные функции, каждая из которых имеет свои особенности.",
+      },
+    ],
+  },
+  {
+    sections: [
+      {
+        heading: "Работа с массивами",
+        text: "Массивы позволяют хранить упорядоченные коллекции данных.",
+      },
+      {
+        heading: "Методы массивов",
+        text: "Методы, такие как forEach, map и filter, помогают работать с массивами эффективно и читаемо.",
+      },
+      {
+        heading: "Методы массивов2",
+        text: "Методы, такие как forEach, map и filter, помогают работать с массивами эффективно и читаемо.",
+      },
+    ],
+  },
+];
 
-// fruits.forEach((fruit, index) => {
-//   console.log(`${index + 1}. ${fruit}`);
-// });
+const buttonsContainer = document.querySelector(".modal__tabs");
+const contentContainer = document.querySelector(".modal__bottom-content");
+const allButtons = buttonsContainer.querySelectorAll("button");
 
-const arr1 = [1, 5, 8];
-const arr2 = [1, 5, 8];
-const arr3 = arr1;
-const x = 4;
-const p = "Lorem ipsum, dolor sit amet consectetur adipisi";
-const h3 = "Курсы и тренинги";
+buttonsContainer.addEventListener("click", (e) => {
+  // if (e.target) console.log(e.target.tagName);
+  if (e.target && e.target.tagName === "BUTTON") {
+    const ind = e.target.getAttribute("data-ind");
+    // console.log(ind);
+    showTab(ind);
+  }
+});
 
-const str = "wer" + x;
-const str2 = "Курсы \"и\" 'тренинги'";
-const str3 = ` 
-    <p>
-        ${p}
-    </p>
-    <h3>${h3}</h3>
-`;
-console.log(str2);
+function showTab(index) {
+  let data = "";
+  tabsData[index].sections.forEach((item) => {
+    data += `
+            <h3>${item.heading}</h3>
+            <p>${item.text}</p>
+        `;
+  });
+  // console.log(data);
+
+  contentContainer.innerHTML = data;
+
+  // обновление стилей табов
+  allButtons.forEach((btn) => btn.classList.remove("active"));
+  allButtons[index].classList.add("active");
+}
+
+// первый таб по умолчанию
+showTab(0);
